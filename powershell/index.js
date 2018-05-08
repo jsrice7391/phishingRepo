ee95ae1eaaf3adeebadcb1daf61bdaac876f2a5d
Justin Rice <jsrice7391@gmail.com>
2018-05-08 18:41:52 -0400

    Trying to work on finding how to run powershell or anything else

---
+++ powershell/index.js
@@ -0,0 +1,24 @@
+const PowerShell = require("powershell");
+
+// Start the process
+let ps = new PowerShell("echo 'powershell is awesome'");
+
+// Handle process errors (e.g. powershell not found)
+ps.on("error", err => {
+    console.error(err);
+});
+
+// Stdout
+ps.on("output", data => {
+    console.log(data);
+});
+
+// Stderr
+ps.on("error-output", data => {
+    console.error(data);
+});
+
+// End
+ps.on("end", code => {
+    // Do Something on end
+});
